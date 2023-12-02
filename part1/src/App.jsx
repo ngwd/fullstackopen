0b6990041850a85fc9c411529690f3862027face
ngwd <ngwuedag@gmail.com>
2023-12-02 10:51:59 -0800

    1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,29 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+const Button = ({clickHandler, text})=> (<button onClick={clickHandler}> {text} </button>);
+const Display = ({text, count}) =>(<p>{text} {count} </p>);
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+ 
+  const positiveClick = ()=> setGood(good+1);
+  const negativeClick = ()=> setBad(bad+1);
+  const neutralClick  = ()=> setNeutral(neutral+1);
+
   return (
-    <div>{counter}</div>
+    <div>
+      <h2>Give feedback</h2>
+      <Button clickHandler={positiveClick} text='good' />
+      <Button clickHandler={neutralClick}  text='neutral' />
+      <Button clickHandler={negativeClick} text='bad' />
+      <h2>Statistics</h2>
+      <Display text='good' count={good}/>
+      <Display text='neutral' count={neutral}/>
+      <Display text='bad' count={bad}/>
+    </div>
   )
 }
 
