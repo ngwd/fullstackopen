da75a2de29bf6a32a3d86bd19a2c2c865cc2bccc
ngwd <ngwuedag@gmail.com>
2023-12-01 19:12:52 -0800

    15:step5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,35 +1,37 @@
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  const Header = (props)=>(<h1>{props.course}</h1>)
-  const Content = ()=> {
-    const contents = parts.map((part) => (<p key={part.name}>{part.name} {part.exercises}</p>));
+  const Header = (props)=>(<h1>{props.course.name}</h1>)
+  const Content = (props)=> {
+    const contents = props.course.parts.map((part) => (<p key={part.name}>{part.name} {part.exercises}</p>));
     return <div> {contents} </div>;
   } 
-  const Total = ()=>{
-    const t = parts.reduce((acc, item)=>acc+item.exercises, 0);
+  const Total = (props)=>{
+    const t = props.course.parts.reduce((acc, item)=>acc+item.exercises, 0);
     return <p>Number of exercises {t} </p>;
   }
 
   return (
     <div>
       <Header course={course}/>
-      <Content />
-      <Total />
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
