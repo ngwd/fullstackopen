29a8126c6363a3d03c67602ed29bc2666ffb0979
ngwd <ngwuedag@gmail.com>
2023-12-01 19:06:36 -0800

    14: step 4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,23 +1,27 @@
 const App = () => {
   const course = 'Half Stack application development'
 
-  const part1 = {name: 'Fundamentals of React', exercises:10};
-  const part2 = {name: 'Using props to pass data', exercises: 7};
-  const part3 = {name: 'State of a component', exercises: 14};
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   const Header = (props)=>(<h1>{props.course}</h1>)
-  const Part = (props)=>{
-     return <p>{props.part.name} {props.part.exercises}</p>
-  };
-  const Content = ()=> (
-    <div>
-      <Part part={part1} />
-      <Part part={part2} />
-      <Part part={part3} />
-    </div>
-  );
+  const Content = ()=> {
+    const contents = parts.map((part) => (<p key={part.name}>{part.name} {part.exercises}</p>));
+    return <div> {contents} </div>;
+  } 
   const Total = ()=>{
-    const t = part1.exercises + part2.exercises + part3.exercises;
+    const t = parts.reduce((acc, item)=>acc+item.exercises, 0);
     return <p>Number of exercises {t} </p>;
   }
 
