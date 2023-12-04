21608cea565e94890fba35728376f36ea34d47b2
ngwd <ngwuedag@gmail.com>
2023-12-04 14:21:33 -0800

    2.15: add update functionality to phonebook

--- part2-phonebook/src/components/PersonForm.jsx
+++ part2-phonebook/src/components/PersonForm.jsx
@@ -8,16 +8,44 @@ const PersonForm = ({persons, updatePersons}) => {
   const handleNameChange = (event)=>{ setNewName(event.target.value); };
   const handleNewNumberChange = (event)=>{ setNewNumber(event.target.value); };
 
-  const nameExists = (name) => {
-    let p = persons.filter(person=>person.name===name);
-    return p.length>0;
+  const checkPersonEnum = {
+    only_name_exists:0,
+    perfect_matched_exists:1,
+    no_such_entry:2,
+  };
+  const checkPerson = (name, number) => {
+    let personsSameName = persons.filter(person=>person.name===name);
+    if (personsSameName.length > 0) {
+      let personsSamePhone = personsSameName.filter(person=>person.number===number);
+      if (personsSamePhone.length>0) {
+        return [checkPersonEnum.perfect_match_exists, -1];
+      }
+      return [checkPersonEnum.only_name_exists, personsSameName[0].id];
+    }
+    return [checkPersonEnum.no_such_entry, -1];
   };
 
   const addNewPerson = (event)=>{
     event.preventDefault(); 
-    if (nameExists(newName)) {
+    const [checkResult, id] = checkPerson(newName, newNumber);
+    if (checkResult == checkPersonEnum.perfect_matched_exists) {
       alert(`${newName} is already added to phonebook`);
-    } else {
+    }
+    else if (checkResult == checkPersonEnum.only_name_exists) {
+      if(window.confirm(`${newName} is already added to phonebook, replace the old number with new one?`) === false) return;  
+      let personWithNewNumber = {name:newName, number:newNumber, id};
+      Phonebook.update(id, personWithNewNumber)
+      .then( data=> {
+        console.log(data);
+        updatePersons(persons.map(person => person.id===id ? data : person));
+        setNewName('');
+        setNewNumber('');
+      })
+      .catch( error =>{
+        console.log(error);
+      });
+    }
+    else {
       let newPerson = {name:newName, number:newNumber, id:persons.length+1};
       Phonebook.addOne(newPerson)
       .then( data=>{
