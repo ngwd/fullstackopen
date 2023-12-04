5340e8d08c067c7e31de389bbda63776deebca89
ngwd <ngwuedag@gmail.com>
2023-12-03 19:56:23 -0800

    2.11

--- part2-phonebook/src/App.jsx
+++ part2-phonebook/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  
+  const [persons, setPersons] = useState([]);
   const updatePersons=(p)=>setPersons(p);
-
   const [newFilter, setNewFilter] = useState('');
   const updateFilter=(f)=>setNewFilter(f);
+
+  const hook = ()=>{
+    axios.get("http://localhost:3001/persons")
+    .then(response=>{
+      setPersons(response.data);
+    });
+  };
+  useEffect(hook, []);
 
   return (
     <div>
