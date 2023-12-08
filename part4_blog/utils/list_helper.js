902d59349e2a4432ca32aa73a0ac14e690dd8f39
ngwd <ngwuedag@gmail.com>
2023-12-07 17:46:28 -0800

    4.3~4.7 jest

---
+++ part4_blog/utils/list_helper.js
@@ -0,0 +1,60 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const t = blogs.reduce((total, cur)=>total + cur.likes, 0)
+  return t
+}
+const favoriteBlog=(blogs) => {
+  let pos = 0;
+  let likes = -1;
+  for(let i = 0; i < blogs.length; ++i) {
+    if (blogs[i].likes > likes) {
+      pos = i;
+      likes = blogs[i].likes; 
+    }
+  }
+  const {title, author} = blogs[pos];
+  return {title, author, likes};
+}
+
+const mostBlogs = (blogs)=> {
+  const count = {};
+  let result = {author:"", blogs:-1};
+  blogs.forEach(blog => {
+    count[blog.author] = (count[blog.author] ?? 0) + 1;
+    if (count[blog.author] > result.blogs) {
+      result = {author: blog.author, 
+        blogs: count[blog.author]
+      }
+    }
+  })
+  return result
+}
+
+const mostLikes = (blogs)=>{
+  const authorLikes = {}
+  let result = {author:"", likes:-1}
+  for(i in blogs) {
+    let {author, likes} = blogs[i]
+    if (authorLikes[author] === undefined) {
+      authorLikes[author] = likes
+    }
+    else {
+      authorLikes[author] += likes
+    }
+    likes = authorLikes[author] 
+    if (likes > result.likes) {
+      result = {author, likes}
+    }
+  }
+  return result;
+}
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostLikes,
+  mostBlogs
+}
