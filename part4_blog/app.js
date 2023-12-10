afb03970de5e0695889905a1298d6f69463c8fa4
ngwd <ngwuedag@gmail.com>
2023-12-09 16:43:57 -0800

    4.8: refactor index.js, separate app.js for integration test

---
+++ part4_blog/app.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const blogRouter = require('./controllers/blogs')
+
+const mongoUrl = process.env.MONGODB_URI 
+mongoose.connect(mongoUrl).then(
+  console.log('db conncted')
+)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports=app
+
--- part4_blog/index.js
+++ part4_blog/index.js
@@ -1,19 +1,5 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogRouter = require('./controllers/blogs')
 require('dotenv').config()
-
-const mongoUrl = process.env.MONGODB_URI 
-mongoose.connect(mongoUrl).then(
-  console.log('db conncted')
-)
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogRouter);
-
+const app = require('./app')
 const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
