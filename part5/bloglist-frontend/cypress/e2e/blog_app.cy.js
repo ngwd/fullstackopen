43b3d68e97bcd7fbe0cfe14bf58b7f9d68d8a597
ngwd <ngwuedag@gmail.com>
2023-12-20 09:10:45 -0800

    5.20 e2e like button

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -2,7 +2,6 @@ describe('blog app', () => {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:5173')
-
   })
   it('login form shown', function() {
     cy.contains('login to application')
@@ -17,27 +16,55 @@ describe('blog app', () => {
       cy.get('#login-button').click() 
       cy.contains('wuedak ng logged in')
     })
+
     it('failure in login', function() {
+      cy.get('#login-button').click() 
       cy.get('#username').type('ngwd') 
       cy.get('#password').type('fulslstack') 
       cy.get('#login-button').click() 
+
       cy.contains('invalid password or user name')
-      cy.get('#notification-label').should('exist')
-      cy.get('#notification-label')
-        .should('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.get('.error')
+        .should('contain', 'invalid password or user name')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
       cy.wait(4000)
-      cy.get('#notification-label').should('not.exist')
+      cy.get('.error').should('not.exist')
     })
   })
 
-  it('new blog can be created', function() {
-    cy.get('#show-new-blog-form-button').click()
-    cy.get('#hide-new-blog-form-button').click()
-    cy.get('#show-new-blog-form-button').click()
-    cy.get('#title-input').type('Batanm\'s war')
-    cy.get('#author-input').type('zojiritsu')
-    cy.get('#url-input').type('http://localhost')
-    cy.get('#new-button').click()
-    cy.contains('Batanm\'s war by zojiritsu')
+  describe('When login', function() {
+    beforeEach(function() {
+      // cy.login({userName:'ngwd', password:'fullstack'})
+      cy.get('#username').type('ngwd') 
+      cy.get('#password').type('fullstack') 
+      cy.get('#login-button').click() 
+    })
+    it('new blog can be created and deleted, operated on webpage', function() {
+      cy.get('#show-new-blog-form-button').click()
+      cy.get('#hide-new-blog-form-button').click()
+      cy.get('#show-new-blog-form-button').click()
+      cy.get('#title-input').type('Batanm\'s war')
+      cy.get('#author-input').type('zojiritsu')
+      cy.get('#url-input').type('http://localhost')
+      cy.get('#new-button').click()
+      cy.contains('Batanm\'s war by zojiritsu').find('button').contains('view').click()
+      cy.contains('Batanm\'s war by zojiritsu').parent()
+      cy.contains('remove').click()
+      cy.contains('Batanm\'s war by zojiritsu').should('not.exist')
+    })
+  })
+  describe('Test the like', function() {
+    beforeEach(function() {
+      cy.login({userName:'ngwd', password:'fullstack'})
+    })
+    it('create new blog and like it', function() {
+      cy.createNewBlog({title:'Batanm\'s war', author:'zojiritsu', url:'http://localhost'})
+      cy.contains('Batanm\'s war by zojiritsu').as('blog')
+      cy.get('@blog').find('button').contains('view').click()
+      cy.get('@blog').parent().as('parent')
+      cy.get('@parent').contains('like 0')
+      cy.get('@parent').find('button').contains('like').click()
+      cy.get('@parent').contains('like 1')
+    })
   })
 })
--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -22,4 +22,23 @@
 //
 //
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+let backendUrl = 'http://localhost:3003'
+let frontendUrl = 'http://localhost:5173'
+Cypress.Commands.add('login', ({ userName, password }) => {
+  cy.request('POST', `${backendUrl}/api/login`, { userName, password })
+    .then( res => {
+      localStorage.setItem('loggedBlogAppUser', JSON.stringify(res.body))
+      cy.visit(`${frontendUrl}`)
+    })
+})
+Cypress.Commands.add('createNewBlog', ({ title, author, url }) => {
+  cy.request({
+    url: `${backendUrl}/api/blogs`,
+    method: 'POST',
+    body: { title, author, url },
+    headers: {
+      'Authorization': `Bearer ${ JSON.parse(localStorage.getItem('loggedBlogAppUser')).token }`
+    }
+  })
+  cy.visit(`${frontendUrl}`)
+})
