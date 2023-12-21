0ca934891309690f895319b07eadb6631ea87a5d
ngwd <ngwuedag@gmail.com>
2023-12-21 08:23:10 -0800

    5:21, 5:22

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,3 +1,5 @@
+import blogs from "../../src/services/blogs"
+
 describe('blog app', () => {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
@@ -59,12 +61,39 @@ describe('blog app', () => {
     })
     it('create new blog and like it', function() {
       cy.createNewBlog({title:'Batanm\'s war', author:'zojiritsu', url:'http://localhost'})
-      cy.contains('Batanm\'s war by zojiritsu').as('blog')
+      cy.contains('Batanm\'s war by zojiritsu').as( 'blog')
       cy.get('@blog').find('button').contains('view').click()
       cy.get('@blog').parent().as('parent')
       cy.get('@parent').contains('like 0')
       cy.get('@parent').find('button').contains('like').click()
       cy.get('@parent').contains('like 1')
+    })
+  })
+  describe('Test delete button coexist with the login user', function() {
+    beforeEach(function() {
+      cy.login({userName:'ngwd', password:'fullstack'})
+    })
+    it('create new blog and see the button of remove and user name', function() {
+      cy.get('div.blog').each(($blog) => {
+        cy.wrap($blog).should($unExpandedBlog => {
+          expect($unExpandedBlog.find('button')).to.have.length.lessThan(2)
+        })
+
+        cy.wrap($blog).find('button').contains('view').click()
+
+        cy.wrap($blog).should($expandedBlog => {
+          expect($expandedBlog.find('button')).to.have.length.greaterThan(2)
+          let $removeButton = $expandedBlog.find('button:contains("remove")')
+          let removeButtonInvisible = $removeButton.css('display') === 'none'
+
+          let $ps = $expandedBlog.find('p')
+          let hasSpecificName = $ps.toArray().some(p => p.innerText.trim() === 'wuedak ng') 
+
+          console.log(removeButtonInvisible, hasSpecificName)
+          expect(removeButtonInvisible).not.equal(hasSpecificName) 
+        })
+        cy.wrap($blog).find('button').contains('hide').click()
+      })
     })
   })
 })
