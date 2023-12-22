c986d8fcf5935f87a448b479c05b09fb4e44eb37
ngwd <ngwuedag@gmail.com>
2023-12-21 20:54:06 -0800

    6.3: make vote working

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,23 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
-
+  switch(action.type) {
+    case 'VOTE':
+      {
+        const id = action.payload.id
+        const anecdoteToChange = state.find(n => n.id === id)
+        const changedAnecdote = {...anecdoteToChange, votes: anecdoteToChange.votes+1}
+        return state.map(a => a.id !== id ? a : changedAnecdote)
+      }
+  }
   return state
 }
-
+export const vote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: {
+      id
+    }
+  }
+}
 export default reducer
