168e996d08930f699007ccae27a55835116dd6b7
ngwd <ngwuedag@gmail.com>
2023-12-25 13:12:37 -0800

    6.14: service to get all of the data

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -2,9 +2,11 @@ import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import anecReducer from './reducers/anecdoteReducer'
+import anecReducer, { setAnecdotes } from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
+import anecdoteService from './services/anecdotes' 
+import anecdotes from './services/anecdotes'
 
 const store = configureStore({
   reducer: {
@@ -14,6 +16,9 @@ const store = configureStore({
   }
 })
 // const reducer = combineReducers( { anecReducer, filterReducer })
+anecdoteService.getAll().then(anecdotes => {
+  store.dispatch(setAnecdotes(anecdotes))
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const anecdotesAtStart = [
+  /*
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  */
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -35,11 +37,14 @@ const anecSlice = createSlice({
       const anec = asObject(action.payload)
       state.push(anec)
     },
+    setAnecdotes(state, action) {
+      return action.payload
+    }
   }
 })
 /*
 export const vote = (id) => { return { type: 'VOTE', payload: { id } } }
 export const add = (content) => { return { type: 'ADD', payload: { content } } }
 */
-export const { add, vote } = anecSlice.actions
+export const { add, vote, setAnecdotes } = anecSlice.actions
 export default anecSlice.reducer
