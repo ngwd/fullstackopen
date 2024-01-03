9c21ab1c6a6c053819b6a24d6d659a17085e0e83
ngwd <ngwuedag@gmail.com>
2024-01-02 22:05:48 -0800

    7.8: ultimate hook

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -18,14 +18,20 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
-
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources([...resources, response.data])
   }
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
+  useEffect(() => {
+    getAll()
+  }, [baseUrl])
 
   const service = {
-    create
+    getAll, create
   }
 
   return [
