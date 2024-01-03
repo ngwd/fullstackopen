8cdc52ff9909d828395421f981c7d5d25a30686a
ngwd <ngwuedag@gmail.com>
2024-01-02 17:09:56 -0800

    7.5~7.6: Reset button for form, onReset for useField hook

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -77,10 +77,17 @@ const CreateNew = (props) => {
     navigate('/')
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.onReset()
+    info.onReset()
+    author.onReset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
           <input name='content' {...content} />
@@ -93,7 +100,7 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button> <button type='reset'>reset</button>
       </form>
     </div>
   )
--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -2,12 +2,19 @@ import { useState } from 'react'
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
+
   const onChange = (e) => {
     setValue(e.target.value)
   }
+
+  const onReset = (e) => {
+    setValue('')
+  }
+
   return {
     type,
     value,
-    onChange
+    onChange,
+    onReset
   }
 }
