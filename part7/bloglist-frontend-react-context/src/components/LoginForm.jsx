6e94677864972af7f5931096e9d3948c998510a2
ngwd <ngwuedag@gmail.com>
2024-01-13 17:18:41 -0800

    Import fix: when useLogin in ongoing, it not yet synced the user in useUser, thus will trigger exception when use user directly, put it to useEffect looks better.

--- part7/bloglist-frontend-react-context/src/components/LoginForm.jsx
+++ part7/bloglist-frontend-react-context/src/components/LoginForm.jsx
@@ -1,8 +1,9 @@
 import blogService from '../services/blogs'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useUser, useLogin } from '../AuthenticationContext'
 import { useNotifier } from '../NotificationContext'
+import Notification from './Notification'
 
 const LoginForm = () => {
   const [userName, setUserName] = useState('ngwd')
@@ -12,16 +13,23 @@ const LoginForm = () => {
   const login = useLogin()
   const notifyWith = useNotifier()
 
+  useEffect(() => {
+    // This effect will be triggered whenever the user state changes
+    if (user && user.name) {
+      notifyWith(`${user.name} logged in`);
+      blogService.setToken(user.token);
+    }
+  }, [user]);
+
   const handleLogin = async (e) => {
     e.preventDefault()
     try {
       await login({ userName, password })
-      // setUser(user)
-
-      notifyWith(`${user.name} logged in`)
+      // notifyWith(`${user.name} logged in`)
       blogService.setToken(user.token)
     }
     catch (exception) {
+      console.log('ex', exception)
       notifyWith('invalid password or user name')
     }
     finally {
@@ -35,6 +43,7 @@ const LoginForm = () => {
         user ? null : 
         (
           <form onSubmit={handleLogin}>
+            <Notification />
             <h2> login to application </h2>
             <div>
               user name <input type='text' id='username' value={userName} onChange={ ({ target }) => setUserName(target.value) } />
