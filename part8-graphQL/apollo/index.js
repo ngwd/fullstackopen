b84d0af0dfc2f08038eeaf705c93bad5f94b1978
ngwd <ngwuedag@gmail.com>
2024-01-20 22:22:46 -0800

    8.14 query improvement for allBooks, with $all, <schemaName>.collection.countDownDocument(), find, findOne

--- part8-graphQL/apollo/index.js
+++ part8-graphQL/apollo/index.js
@@ -17,93 +17,6 @@ mongoose.connect(MONGODB_URI)
   console.log('error during connecting', error.message)
 })
 
-let authors = [
-  /*
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-  */
-]
-
-/*
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
-*/
-
-let books = [
-  /*
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-  */
-]
-
 const typeDefs = `
   type Author {
     name: String!
@@ -153,10 +66,8 @@ const getBookCountCache = () => {
 }
 const resolvers = {
   Query: {
-    authorCount: () => authors.length,
-    bookCount: () => books.length,
-    // allAuthors: (_, { refreshCache = false }) => {
-    // allAuthors: (_, { refreshCache }) => {
+    authorCount: () => Author.collection.countDocuments(),
+    bookCount: () => Book.collection.countDocuments(),
     allAuthors: (root, args) => {
       console.log(args)
       if (args.refreshCache) {
@@ -164,22 +75,20 @@ const resolvers = {
       }
       return authors.map(a => ({ ...a, bookCount: bookCountCache?.[a.name]??0 }))
     },
-    allBooks: (root, args) => {
-      return books.filter(b => 
-        (!args.author || b.author === args.author) && 
-        (!args.genre  || b.genres.includes(args.genre)))
-
-      /*
-      if (!args.author && !args.genre)
-        return books
-      else if (args.author && !args.genre)
-        return books.filter(b => b.author === args.author)
-      else if (!args.author && args.genres)
-        return books.filter(b => b.genres.includes(args.genre))
-      else 
-        return books.filter(b => b.genres.includes(args.genre) && b.author === args.author)
-      */
-    }
+    allBooks: async (root, args) => {
+      const conditions = {}
+      if (args.author) {
+        const author = await Author.findOne({ name: args.author})
+        if (author) {
+          conditions["author"] = author._id.toString() 
+        }
+      }
+      if (args.genre) {
+        conditions["genres"] = {"$all" : [args.genre]}
+      }
+      const books = await Book.find(conditions)
+      return books
+    },
   },
   Mutation: {
     addAuthor: (root, args) => {
