fe1eb462721ab78cd71cbe430af942c3c9446603
ngwd <ngwuedag@gmail.com>
2024-05-20 11:33:57 -0700

    trivial improvement

--- vanilla-js-mvc-games/2048/2048.js
+++ vanilla-js-mvc-games/2048/2048.js
@@ -1,45 +1,79 @@
 // model
 CANVAS_SIZE = 600;
-CANVAS_BGCOLOR = "#333333";
+CANVAS_BGCOLOR = "#EEEEEE";
 GAME_SIZE = 4;
 
 BLOCK_SIZE = 130;
 BLOCK_MARGIN = (CANVAS_SIZE - GAME_SIZE*BLOCK_SIZE) / (GAME_SIZE+1);
-// BLOCK_BGCOLOR = "#111111";
-BLOCK_BGCOLOR = "#555555";
-BLOCK_FGCOLOR = "#621222";
+BLOCK_FGCOLOR = "#7AADCC";
+BLOCK_BGCOLOR = "#CCCCCC";
 GAME_SIZE = 4;
 
 // random int in [a, b)
 randomInt = (a, b) => a + Math.floor(Math.random()*(b -a));
 randomChoice = (arr) => arr[randomInt(0, arr.length)];
 
-allPositions = ((rows, cols) => {
+// get [[0,0], [0,1]...,[0, n]
+//      [1,0], [1,1]...
+//      ...
+//      [m, 0], [m,2]...
+//     ]
+
+// range(5)   => [0, 1, 2, 3, 4];   range(2,5) => [2, 3, 4]; 
+range = (lo, hi) => {
+  if (hi === undefined) {
+    hi = lo;
+    lo = 0; 
+  }
+  if (lo >= hi) return []; 
+  return [...Array(hi-lo).keys()].map(i=>i+lo);
+};
+R = range(GAME_SIZE);
+C = range(GAME_SIZE);
+crossProduct = (rows, cols) => rows.flatMap(i => cols.map(j => [i,j]));
+allCoordinates = crossProduct(R, C);
+
+// nullMatrix = (rows, cols) => rows.Map(i => cols.map(j => null));
+// getNullMatrix = (rows, cols) => nullMatrix(arr, arr);
+/*
+allCoordinates = ((rows, cols) => {
   return Array(rows).fill().map((_, x) => 
     Array(cols).fill().map((_, y) => [x, y])
   ).flat();
 })(GAME_SIZE, GAME_SIZE);
+*/
+
+getNullMatrix = ((rows, cols) => {
+  // return Array(GAME_SIZE).fill(Array(GAME_SIZE).fill(null)) // it is buggy with this line, 
+  return Array.from({length:rows}, ()=>Array(cols).fill(null));
+});
 
 class Game {
   constructor() {
     this.data = [];
     this.initializeData();
-    
   }
+
   initializeData() {
-    // this.data = Array(GAME_SIZE).fill(Array(GAME_SIZE).fill(null)) // it is buggy with this line, 
-    this.data = Array.from({length:GAME_SIZE}, ()=>Array(4).fill(null)); // should use this line instead
+    this.data = getNullMatrix(GAME_SIZE, GAME_SIZE)
     this.generateNewBlock(); 
     this.generateNewBlock(); 
   }
 
   generateNewBlock () {
-    let tmp = allPositions.filter(([X, Y]) => this.data[X][Y] === null)
+    let tmp = allCoordinates.filter(([x, y]) => this.data[x][y] === null)
     if (tmp.length===0) return;
 
     let [x, y] = randomChoice(tmp);
     console.log(x, y)
     this.data[x][y] = 2;
+  }
+
+  // {2, n, 2, n} => {4, n, n, n}
+  // {2, 2, 2, n} => {4, 2, n, n}
+  // {2, 2, 2, 2} => {4, n, 4, n}
+  shiftBlock(arr) {
+
   }
 }
 
@@ -86,6 +120,14 @@ class View {
   }
   drawNumBlock(i, j, num) {
     let span = document.createElement("span")
+    span.style.fontSize = 40
+    span.style.fontFamily = "Lucida Console"
+    span.style.color = "#FFFFFF"
+
+    // span.style.position = "relative";
+    // span.style.top = 0.5; span.style.left = 0.5;
+    // span.style.transform = translate(-0.5, -0.5);
+
     let text = document.createTextNode(num)
     span.appendChild(text)
     let block = this.drawBlock(i, j, BLOCK_FGCOLOR)
