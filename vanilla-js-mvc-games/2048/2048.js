5e78550fbddf2a90f1498e2c6c06dec586d83f9e
ngwd <ngwuedag@gmail.com>
2024-05-19 18:04:41 -0700

    Add number blocks

--- vanilla-js-mvc-games/2048/2048.js
+++ vanilla-js-mvc-games/2048/2048.js
@@ -1,13 +1,14 @@
 // model
-CANVAS_SIZE = 600
-CANVAS_BGCOLOR = "#333333"
-GAME_SIZE = 4
+CANVAS_SIZE = 600;
+CANVAS_BGCOLOR = "#333333";
+GAME_SIZE = 4;
 
-BLOCK_SIZE = 130
-BLOCK_MARGIN = (CANVAS_SIZE - GAME_SIZE*BLOCK_SIZE) / (GAME_SIZE+1)
-// BLOCK_BGCOLOR = "#111111"
-BLOCK_BGCOLOR = "#555555"
-GAME_SIZE = 4
+BLOCK_SIZE = 130;
+BLOCK_MARGIN = (CANVAS_SIZE - GAME_SIZE*BLOCK_SIZE) / (GAME_SIZE+1);
+// BLOCK_BGCOLOR = "#111111";
+BLOCK_BGCOLOR = "#555555";
+BLOCK_FGCOLOR = "#621222";
+GAME_SIZE = 4;
 
 // random int in [a, b)
 randomInt = (a, b) => a + Math.floor(Math.random()*(b -a));
@@ -44,13 +45,15 @@ class Game {
 
 // view
 class View {
-  constructor(container) {
+  constructor(game, container) {
     this.container = container;
+    this.game = game;
     this.initializeContainer();
   }
 
   initializeContainer() {
-    this.container.style.width = this.container.style.height = CANVAS_SIZE;
+    this.container.style.height = CANVAS_SIZE;
+    this.container.style.width =  CANVAS_SIZE;
     this.container.style.backgroundColor = CANVAS_BGCOLOR;
     this.container.style.position = "relative";
     this.container.style.display = "inline-block";
@@ -58,24 +61,40 @@ class View {
   drawGame() {
     for(let i = 0; i<GAME_SIZE; i++) {
       for(let j = 0; j<GAME_SIZE; j++) {
-        this.drawBlockBG(i, j);
+        let num = this.game.data[i][j] 
+        if (num === null) {
+          this.drawBlock(i, j, BLOCK_BGCOLOR)
+        }
+        else {
+          this.drawNumBlock(i, j, num)
+        }
+        //  else 
+        //  this.drawBlock(i, j, this.game.data[i][j]);
       }
     }
   }
-  drawBlockBG (i, j) {
+  drawBlock (i, j, color) {
     let block = document.createElement("div")
     block.style.width = BLOCK_SIZE;
     block.style.height = BLOCK_SIZE;
-    block.style.backgroundColor = BLOCK_BGCOLOR;
+    block.style.backgroundColor = color;
     block.style.position = "absolute";
     block.style.top = i * BLOCK_SIZE + (i+1)*BLOCK_MARGIN;
     block.style.left = j * BLOCK_SIZE + (j+1)*BLOCK_MARGIN;
     this.container.append(block);
+    return block;
+  }
+  drawNumBlock(i, j, num) {
+    let span = document.createElement("span")
+    let text = document.createTextNode(num)
+    span.appendChild(text)
+    let block = this.drawBlock(i, j, BLOCK_FGCOLOR)
+    block.appendChild(span)
   }
 }
 
 // controller
 var container = document.getElementById("game-container");
-var view = new View(container);
 var game = new Game();
+var view = new View(game, container);
 view.drawGame();
