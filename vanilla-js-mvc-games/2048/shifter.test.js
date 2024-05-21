9789e8cd17c9b5413808202eebc8627af1c8b419
ngwd <ngwuedag@gmail.com>
2024-05-21 11:10:46 -0700

    fix an error in moving null to tail, add test cases and jest, make unit test works

--- vanilla-js-mvc-games/2048/shifter.test.js
+++ vanilla-js-mvc-games/2048/shifter.test.js
@@ -1,23 +1,52 @@
-const { beforeEach } = require('node:test');
-const { describe } = require('yargs')
-const shiftArray = require('./shifter')
+// import { shiftArray } from './shifter.js';
+const { shiftArray } = require('./shifter.js');
 
-  // {2, n, 2, n} => {4, n, n, n}
-  // {2, n, n, 2} => {4, n, n, n}
-
-  // {2, 2, 2, n} => {4, 2, n, n}
-  // {4, 2, 2, n} => {4, 4, n, n}
-  // {2, 2, 4, n} => {4, 4, n, n}
-
-  // {2, 2, 2, 2} => {4, 4, n, n}
-  // {2, 2, 4, 2} => {4, 4, 2, n}
   // {2, 4, 2, 2} => {2, 4, 4, n}
 describe ('shiftArray', ()=>{
-
-  beforeEach(()=>{})
   test("{2, n, 2, n} => {4, n, n, n} ", ()=> {
     arr = [2, null, 2, null];
     shiftArray(arr);
-    expect(arr).toBe([4, null, null, null]);
+    expect(arr).toStrictEqual([4, null, null, null]);
+  });
+  test("{2, n, n, 2} => {4, n, n, n} ", ()=> {
+    arr = [2, null, null, 2];
+    shiftArray(arr);
+    expect(arr).toStrictEqual([4, null, null, null]);
+  });
+
+  test("{2, 2, 2, n} => {4, 2, n, n} ", ()=> {
+    arr = [2, 2, 2, null];
+    shiftArray(arr);
+    expect(arr).toStrictEqual([4, 2, null, null]);
+  });
+
+  test("{4, 2, 2, n} => {4, 4, n, n} ", ()=> {
+    arr = [4, 2, 2, null];
+    shiftArray(arr);
+    expect(arr).toStrictEqual([4, 4, null, null]);
+  });
+
+  test("{2, 2, 4, n} => {4, 4, n, n} ", ()=> {
+    arr = [2, 2, 4, null];
+    shiftArray(arr);
+    expect(arr).toStrictEqual([4, 4, null, null]);
+  });
+
+  test("{2, 2, 2, 2} => {4, 4, n, n} ", ()=> {
+    arr = [2, 2, 2, 2];
+    shiftArray(arr);
+    expect(arr).toStrictEqual([4, 4, null, null]);
+  });
+
+  test("{2, 2, 4, 2} => {4, 4, 2, n} ", ()=> {
+    arr = [2, 2, 4, 2];
+    shiftArray(arr);
+    expect(arr).toStrictEqual([4, 4, 2, null]);
+  });
+
+  test("{2, 4, 2, 2} => {2, 4, 4, n} ", ()=> {
+    arr = [2, 4, 2, 2];
+    shiftArray(arr);
+    expect(arr).toStrictEqual([2, 4, 4, null]);
   });
 });
