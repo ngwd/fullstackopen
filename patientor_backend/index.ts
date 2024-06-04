a778e3ab917688e84f160b3b62194f83083c051f
ngwd <ngwuedag@gmail.com>
2024-06-04 10:22:50 -0700

    9.8~9.9 initialize the backend of patientor

---
+++ patientor_backend/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+const cors  = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+const PORT = 3001;
+app.get('/api/ping', (_req, res) => {
+  console.log("ping!");
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
